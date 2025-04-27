2376f92b3692e0bfc722a4b9f66832501d1f6d7d
Tarek Hesham <tarekghost6@gmail.com>
2025-04-27 18:23:25 +0300

    [Quize] - Register routes to api.php with middleware.

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,13 @@
 <?php
 
-use Illuminate\Http\Client\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\QuizController;
+use App\Http\Controllers\QuizSubmissionController;
+use App\Http\Middleware\EnsureQuizActive;
 
+Route::get('quizzes', [QuizController::class, 'index']);
+
+Route::middleware([EnsureQuizActive::class])->group(function () {
+    Route::get('quizzes/{quiz}', [QuizController::class, 'show']);
+    Route::post('quizzes/{quiz}/submissions', [QuizSubmissionController::class, 'submit']);
+});
